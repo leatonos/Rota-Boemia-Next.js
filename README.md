5cd9944551e3c4bab024b841622f19767cf7c853
leatonos <pedrofmattosb@gmail.com>
2023-02-17 13:08:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rota-Boemia-Next.js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-tag86r)
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Rota Boemia</title>
+      </Head>
+
+      <main className={styles.main}></main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
